51799c1aa36fa20ce20fc71f378505e8eb611a04
Alex Candido <alex.candido.tec@gmail.com>
2026-05-12 12:47:58 -0300

    feat: scaffold project structure with route groups and app layout

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+    async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/landing/home',
+        permanent: true,
+      },
+      {
+        source: '/landing',
+        destination: '/landing/home',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
